a52b42a8da4b0559a01b57441f337900c1cbcc00
IkenoXamos <moberlies1337@gmail.com>
2023-12-03 01:36:29 -0500

    Day 3 Part 1 Solution

---
+++ src/day03/index.ts
@@ -0,0 +1,73 @@
+import run from "aocrunner";
+import { findAllIndexes, consecutiveRanges } from '../utils/index.js'
+
+const parseInput = (rawInput: string) => rawInput.split('\n').map((line, y_index) => {
+  // Find all positions of numbers in the line
+  const numberIndices = findAllIndexes(line.split(''), (char) => !!char.match(/[0-9]/));
+  // Within those positions, find all ranges of numbers that are consecutive
+  // These will be used to determine the actual value, combining sequential digits together
+  const numberRanges = consecutiveRanges(numberIndices);
+
+  // We associate the value with each of the positions that number occupied
+  const values = numberRanges.map(range => ({
+    value: parseInt(range.map(index => line[index]).join('')),
+    positions: range.map(index => [index, y_index] as [number, number]),
+  }));
+
+  // Find all of the positions of the symbols in the line
+  const symbols = findAllIndexes(line.split(''), (char) => !!char.match(/[^.0-9]/)).map(index => [index, y_index] as [number, number]);
+
+  return {
+    values,
+    symbols,
+  };
+}).reduce((acc, {values, symbols}) => { // Combine all of the values into a single array for easier processing
+  return {
+    values: acc.values.concat(values),
+    symbols: acc.symbols.concat(symbols),
+  }
+}, { values: [], symbols: [] });
+
+function isNeighbor(target: [number, number], source: [number, number][]): boolean {
+  return source.some(([x, y]) => Math.abs(target[0] - x) <= 1 && Math.abs(target[1] - y) <= 1);
+}
+
+const part1 = (rawInput: string) => {
+  const { values, symbols } = parseInput(rawInput);
+
+  // Out of all of the values in the display, only keep the ones that have at least one symbol adjacent to them
+  // And grab the value of that number
+  const includedNumbers = values.filter(item => item.positions.some(position => isNeighbor(position, symbols))).map(item => item.value);
+
+  // Add up all of the values
+  return includedNumbers.reduce((acc, value) => acc + value, 0);
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -29,3 +29,26 @@
  *     import { myUtil } from '../utils'
  *
  */
+
+export const findAllIndexes = <T>(arr: T[], predicate: (item: T) => boolean): number[] => {
+    return arr.map((item, index) => predicate(item) ? index : undefined).filter(index => index !== undefined) as number[];
+};
+
+export function consecutiveRanges(source: number[]): number[][] {
+  const ranges: number[][] = []; // Hold all of the possible continuous sequences
+  let currentRange: number[] = []; // The current continuous sequence being built
+  source.forEach(item => {
+    if (currentRange.length === 0) { // If this is the beginning of a new sequence
+      currentRange.push(item); // Add it
+    } else if (item - currentRange[currentRange.length - 1] === 1) { // Otherwise, if this is sequential with the previous item
+      currentRange.push(item); // Add it
+    } else { // If it isn't, the previous sequence ends and we start a new one
+      ranges.push(currentRange);
+      currentRange = [item];
+    }
+  });
+  if (currentRange.length > 0) { // Handle edge-case where the final sequence wasn't added yet
+    ranges.push(currentRange);
+  }
+  return ranges;
+}
