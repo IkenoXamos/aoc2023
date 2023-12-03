39f46e2f35449e7b34f0ae386d5e5808e368f233
IkenoXamos <moberlies1337@gmail.com>
2023-12-03 01:57:58 -0500

    Day 3 Part 2 Solution

--- src/day03/index.ts
+++ src/day03/index.ts
@@ -16,17 +16,21 @@ const parseInput = (rawInput: string) => rawInput.split('\n').map((line, y_index
 
   // Find all of the positions of the symbols in the line
   const symbols = findAllIndexes(line.split(''), (char) => !!char.match(/[^.0-9]/)).map(index => [index, y_index] as [number, number]);
+  // Also specifically track the positions of the '*'s for Part 2
+  const gears = findAllIndexes(line.split(''), (char) => !!char.match(/[*]/)).map(index => [index, y_index] as [number, number]);
 
   return {
     values,
     symbols,
+    gears,
   };
-}).reduce((acc, {values, symbols}) => { // Combine all of the values into a single array for easier processing
+}).reduce((acc, { values, symbols, gears }) => { // Combine all of the values into a single array for easier processing
   return {
     values: acc.values.concat(values),
     symbols: acc.symbols.concat(symbols),
+    gears: acc.gears.concat(gears),
   }
-}, { values: [], symbols: [] });
+}, { values: [], symbols: [], gears: [] });
 
 function isNeighbor(target: [number, number], source: [number, number][]): boolean {
   return source.some(([x, y]) => Math.abs(target[0] - x) <= 1 && Math.abs(target[1] - y) <= 1);
@@ -44,9 +48,26 @@ const part1 = (rawInput: string) => {
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  // values includes all of the metadata about the positions/values of the numbers in the display
+  // gears includes all of the metadata about the positions/values of the '*'s in the display
+  const { values, gears } = parseInput(rawInput);
 
-  return;
+  // Out of the '*'s in the display, we only care about the ones that have exactly 2 adjacent numbers
+  const correctGears = gears.map(position => {
+    // We find all of the numbers that are adjacent to this '*'
+    const neighbors = values.filter(item => isNeighbor(position, item.positions));
+
+    // Only if there are exactly 2 numbers adjacent,
+    if (neighbors.length === 2) return neighbors; // Hold on to those numbers metadata
+    return null; // Otherwise return null
+
+    // Filter out the nulls
+    // The typing gets a bit complex here, but its an array of pairs of 'number's
+    // But each 'number' includes its value and a range of positions
+  }).filter(item => item !== null) as { value: number, positions: [number, number][] }[][];
+
+  // From there, calculate and add up the gear ratio of the found gears
+  return correctGears.reduce((acc, gears) => acc + gears[0].value * gears[1].value, 0);
 };
 
 run({
